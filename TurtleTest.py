47ec7325eadcd20abcd6ea5d88ec9626e5956014
sophia <phia.hilbert@gmail.com>
2022-11-10 16:31:03 +0100

    hello

---
+++ TurtleTest.py
@@ -0,0 +1,16 @@
+import turtle
+estela = turtle.Pen()
+estela.shape("turtle")
+estela.forward(100)
+estela.left(90)
+estela.forward(100)
+estela.left(90)
+estela.forward(100)
+estela.left(90)
+estela.forward(100)
+estela.left(90)
+estela.reset()
+for i in range (4):
+    estela.forward(100)
+    estela.left(90)
+
